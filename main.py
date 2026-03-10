5c9cce7be69a1a382faacf762277742cf9191648
Alessio MARINO <amarino_ext@christiandior.com>
2026-03-10 01:07:56 +0100

    Allow CORS origins

--- main.py
+++ main.py
@@ -6,17 +6,37 @@ from rag import ask_question
 
 app = FastAPI()
 
+origins = [
+    "https://marino-ai.vercel.app",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET"],
     allow_headers=["*"],
 )
 
+# @app.get("/")
+# def health_check():
+#     return {"status": "online", "message": "Alessio Marino RAG API is running"}
+
 @app.get("/")
 def health_check():
-    return {"status": "online", "message": "Alessio Marino RAG API is running"}
+    try:
+        freeze_output = subprocess.check_output(["pip", "freeze"]).decode("utf-8")
+        packages = freeze_output.split('\n')
+    except Exception as e:
+        packages = [f"Errore nel recupero pacchetti: {str(e)}"]
+
+    return {
+        "status": "online", 
+        "message": "Alessio Marino RAG API is running",
+        "render_environment_freeze": packages
+    }
 
 @app.get("/chat")
 def chat(q: str):
