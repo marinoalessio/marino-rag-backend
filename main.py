af3cc6317074b37926d633d8134dbb1f3f9a2363
Alessio Marino <marino.alessio9599@gmail.com>
2026-03-10 01:55:20 +0100

    stable version

--- main.py
+++ main.py
@@ -3,40 +3,25 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from rag import ask_question
+import subprocess
 
 app = FastAPI()
 
-origins = [
-    "https://marino-ai.vercel.app",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# @app.get("/")
-# def health_check():
-#     return {"status": "online", "message": "Alessio Marino RAG API is running"}
-
 @app.get("/")
 def health_check():
-    try:
-        freeze_output = subprocess.check_output(["pip", "freeze"]).decode("utf-8")
-        packages = freeze_output.split('\n')
-    except Exception as e:
-        packages = [f"Errore nel recupero pacchetti: {str(e)}"]
+    return {"status": "online", "message": "Alessio Marino RAG API is running"}
 
-    return {
-        "status": "online", 
-        "message": "Alessio Marino RAG API is running",
-        "render_environment_freeze": packages
-    }
+@app.get("/health")
+async def health():
+    return {"status": "online"}
 
 @app.get("/chat")
 def chat(q: str):
